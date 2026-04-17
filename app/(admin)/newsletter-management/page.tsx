1f13ea500a1ce2ab92ab2a0722acf66f3784d3de
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-04-17 17:20:56 +0530

    add Newsletter Management page component

---
+++ app/(admin)/newsletter-management/page.tsx
@@ -0,0 +1,8 @@
+export default function NewsletterManagement() {
+  return (
+    <div>
+      <h1>Newsletter Management</h1>
+      <p>Welcome to the Newsletter Management page!</p>
+    </div>
+  );
+}
