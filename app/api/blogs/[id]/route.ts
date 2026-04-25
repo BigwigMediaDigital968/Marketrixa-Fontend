2c941a5eb4bc105477df4a6c34f0b77f65e43906
Lalit Nath <lalitwebd@bigwigmediadigital.com>
2026-04-25 18:12:08 +0530

    blog api deployment error fir

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -10,7 +10,7 @@ import {
 
 // type Params = { params: Promise<{ id: string }> };
 
-type Params = { params: { id: string } };
+type Params = { params: Promise<{ id: string }> };
 
 export async function GET(_req: NextRequest, { params }: Params) {
   const { id } = await params;
