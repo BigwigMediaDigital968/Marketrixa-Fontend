99d92b8ecb987f95d8d130d4db8790021b85fb62
Lalit Nath <lalitwebd@bigwigmediadigital.com>
2026-04-25 17:56:47 +0530

    meta pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit, Poppins } from "next/font/google";
 import "./globals.css";
 import WhatsappFloat from "./component/website/WhatsappFloat";
+import Script from "next/script";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -38,6 +39,29 @@ export default function RootLayout({
         {children}
 
         <WhatsappFloat />
+
+        <>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '953255090434595');
+fbq('track', 'PageView');`}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+          style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=953255090434595&ev=PageView&noscript=1"
+            />
+          </noscript>
+        </>
       </body>
     </html>
   );
